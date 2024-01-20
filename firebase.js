d73b64448d8b20d1333a10b7c9b4f32b8e43eac9
Preet2612 <108889069+Preet2612@users.noreply.github.com>
2024-01-20 09:15:37 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBQyHBgzf-Zx107vHdNZU2N2SP1h3c3l-0",
+    authDomain: "website-d841a.firebaseapp.com",
+    projectId: "website-d841a",
+    storageBucket: "website-d841a.appspot.com",
+    messagingSenderId: "123506388202",
+    appId: "1:123506388202:web:a0d411ded807fd62bffb93",
+    measurementId: "G-DVKJD628DZ"
+  };
